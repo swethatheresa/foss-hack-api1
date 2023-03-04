9e7259a42b6df7073c46b7ee7b8d98255c68fd99
shreyas0512 <shreyassanthosh362@gmail.com>
2023-03-04 14:57:41 +0530

    new

--- content.js
+++ content.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const JobApplicationSchema = new mongoose.Schema({
     company: String,
     jobtitle: String,
-    jobdescription: String,
     joblocation: String,
     status: 
     {
